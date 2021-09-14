04fb0b039db83e3f54ae6a7073102ec1df0c8014
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-13 23:56:02 -0300

    Migration da tabela 'emails'

---
+++ application/database/migrations/2021_09_14_025251_create_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Domain\Enums\PrivacyEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('username', 45);
+            $table->string('domain', 120);
+            $table->enum('privacy', PrivacyEnum::toValues());
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
