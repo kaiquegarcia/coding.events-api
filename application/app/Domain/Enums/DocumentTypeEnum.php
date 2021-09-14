552347c20fd9d3433ff441fe4efba0c2b08150c5
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-14 00:14:53 -0300

    Criação de Enum que dita os possíveis tipos de documentos

---
+++ application/app/Domain/Enums/DocumentTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self CPF()
+ * @method static self CNPJ()
+ * @method static self RG()
+ */
+class DocumentTypeEnum extends Enum {}
