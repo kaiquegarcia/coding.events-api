10a013b95810fafe542b3e74217cd9afe259e9a7
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-18 06:23:45 -0300

    Implementação do DocumentRepositoryInterface com Eloquent

---
+++ application/app/Infrastructure/Repositories/Eloquent/DocumentEloquentRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Infrastructure\Repositories\Eloquent;
+
+use App\Domain\Entities\Document;
+use App\Domain\Repositories\DocumentRepositoryInterface;
+use App\Infrastructure\EntityModels\DocumentModel;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use JetBrains\PhpStorm\Pure;
+
+class DocumentEloquentRepository extends AbstractEloquentRepository implements DocumentRepositoryInterface
+{
+
+    #[Pure]
+    public function __construct(DocumentModel $model)
+    {
+        parent::__construct($model);
+    }
+
+    protected function prepareEloquentBuilder(array $params): Builder
+    {
+        $query = $this->model::query();
+        if (isset($params['document_type'])) {
+            $query = $query->where('document_type', $params['document_type']);
+        }
+        if (isset($params['value'])) {
+            $query = $query->where('value', $params['value']);
+        }
+        if (isset($params['privacy'])) {
+            $query = $query->where('privacy', $params['privacy']);
+        }
+        return $query;
+    }
+
+    public function all(array $params = []): Collection
+    {
+        return parent::searchEntities($params);
+    }
+
+    public function find(string $id): ?Document
+    {
+        return parent::findEntity($id);
+    }
+
+    public function save(Document $phone): ?Document
+    {
+        return parent::saveEntity($phone);
+    }
+
+    public function delete(string $id): bool
+    {
+        return parent::deleteById($id);
+    }
+
+    public function restore(string $id): bool
+    {
+        return parent::restoreById($id);
+    }
+}
--- application/app/Providers/RepositoryServiceProvider.php
+++ application/app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Domain\Repositories\DocumentRepositoryInterface;
 use App\Domain\Repositories\EmailRepositoryInterface;
 use App\Domain\Repositories\PhoneRepositoryInterface;
+use App\Infrastructure\Repositories\Eloquent\DocumentEloquentRepository;
 use App\Infrastructure\Repositories\Eloquent\EmailEloquentRepository;
 use App\Infrastructure\Repositories\Eloquent\PhoneEloquentRepository;
 use Illuminate\Support\ServiceProvider;
@@ -16,5 +18,6 @@ class RepositoryServiceProvider extends ServiceProvider
 
         $this->app->bind(PhoneRepositoryInterface::class, PhoneEloquentRepository::class);
         $this->app->bind(EmailRepositoryInterface::class, EmailEloquentRepository::class);
+        $this->app->bind(DocumentRepositoryInterface::class, DocumentEloquentRepository::class);
     }
 }
