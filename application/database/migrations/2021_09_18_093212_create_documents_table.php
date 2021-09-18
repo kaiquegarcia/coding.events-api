a0beb1d56ba18010f54eaf8cdecbcfa267a1cddf
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-18 06:35:21 -0300

    Migrations para documents

---
+++ application/database/migrations/2021_09_18_093212_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Domain\Enums\DocumentTypeEnum;
+use App\Domain\Enums\PrivacyEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->enum('document_type', DocumentTypeEnum::toValues());
+            $table->string('value', 45);
+            $table->enum('privacy', PrivacyEnum::toValues());
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
