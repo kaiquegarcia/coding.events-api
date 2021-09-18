bbeb56a9d5dd9af57d6eb05a607a2244bdc2951d
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-18 08:29:09 -0300

    Criação do AddresRepository

---
+++ application/app/Domain/Repositories/AddressRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Repositories;
+
+use App\Domain\Entities\Address;
+use Illuminate\Support\Collection;
+
+interface AddressRepositoryInterface
+{
+    public function all(array $params = []): Collection;
+
+    public function find(string $id): ?Address;
+
+    public function save(Address $phone): ?Address;
+
+    public function delete(string $id): bool;
+
+    public function restore(string $id): bool;
+}
