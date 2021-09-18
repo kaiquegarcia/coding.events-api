fb8850d8b32097390b182bd01bacd1c72184c606
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-18 07:01:13 -0300

    Adicionando assinatura do método getStatusCode no ResponseInterface

--- application/app/Domain/Components/Http/ResponseInterface.php
+++ application/app/Domain/Components/Http/ResponseInterface.php
@@ -4,6 +4,7 @@ namespace App\Domain\Components\Http;
 
 interface ResponseInterface
 {
+    public function getStatusCode(): int;
     public function getString(): string;
     public function getFromJson(): array;
     public function getFromQuery(): array;
