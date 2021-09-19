768a389113010bf4452c5968d2d7f60ddc7b084f
Kaique Garcia <kaiquegarciam@gmail.com>
2021-09-18 21:50:00 -0300

    Migration de UserModels

---
+++ application/database/migrations/2021_09_19_004803_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Domain\Enums\StatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('first_name', 45);
+            $table->string('last_name', 45);
+            $table->string('avatar_url', 255);
+            $table->string('website', 100);
+            $table->longText('bio');
+            $table->enum('status', StatusEnum::toValues());
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
