f7452c8bfaaab0446a4dbf463d8cdfffd10b577c
Kaique Garcia <kaiquegarciam@gmail.com>
2021-10-15 21:09:18 -0300

    Adicionando método de relacionamento em OrganizationModel

--- application/app/Infrastructure/EntityModels/OrganizationModel.php
+++ application/app/Infrastructure/EntityModels/OrganizationModel.php
@@ -5,10 +5,12 @@ namespace App\Infrastructure\EntityModels;
 use App\Domain\Entities\Organization;
 use App\Domain\Enums\OrganizationStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrganizationModel extends AbstractModel
 {
     use HasFactory;
+
     public string $entityClass = Organization::class;
     protected $table = 'organizations';
     protected $fillable = [
@@ -27,4 +29,9 @@ class OrganizationModel extends AbstractModel
     protected $casts = [
         'status' => OrganizationStatusEnum::class . ':nullable',
     ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class, 'owner_id', 'id');
+    }
 }
